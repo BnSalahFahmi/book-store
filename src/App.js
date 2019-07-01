bc2a18bda32db75f0f41a632982403538a24bbff
BnSalahFahmi <fahmii.bensalah@gmail.com>
2019-07-01 14:04:42 +0200

    implement dashboard component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEdit, faTrash, faUser, faList, faSignOutAlt, faPlus, faTachometerAlt, faBook } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+import Dashboard from './components/dashboard';
+
+library.add(faEye);
+library.add(faEdit);
+library.add(faTrash);
+library.add(faUser);
+library.add(faTachometerAlt);
+library.add(faList);
+library.add(faSignOutAlt);
+library.add(faPlus);
+library.add(faBook);
+
+const App = () => {
+
+  const loggedUser = "Fahmi BEN SALAH";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="cont">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+            <img src={logo} width="60" height="60" alt="CodingTheSmartWay.com" />
+          </a>
+          <div className="collpase nav-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link"><FontAwesomeIcon icon='tachometer-alt' /> Dashboard</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/list" className="nav-link"><FontAwesomeIcon icon='book' /> Books</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link"><FontAwesomeIcon icon='plus' /> New Book</Link>
+              </li>
+            </ul>
+            {<ul className="navbar-nav mr-auto pull-right">
+              <li className="navbar-item">
+                <Link to="/about" className="nav-link"><FontAwesomeIcon icon='user' /> {loggedUser}</Link>
+              </li>
+            </ul>}
+          </div>
+        </nav>
+        <div class="content">
+          <Switch>
+              <Route exact path="/" component={Dashboard} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/dashboard.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import reactjs from '../assets/img/reactjs.jpg';
+import reactrouter4 from '../assets/img/reactrouter.jpg';
+import bootstrap4 from '../assets/img/bootstrap.jpg';
+import redux from '../assets/img/redux.png'
+
+export class Dashboard extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div class="container dashboard-container">
+                <div class="row">
+                    <div className="col-md-6 col-lg-3">
+                        <div className="card dashboard-card">
+                            <img src={reactjs} className="card-img" alt="ReactJS" />
+                            <div className="card-block">
+                                <h3 className="card-title">ReactJS</h3>
+                                <p class="card-text">A declarative, efficient, and flexible JavaScript library for building user interfaces </p>
+                                <a href="https://github.com/facebook/react" class="card-link">Github</a>
+                                <a href="https://fr.reactjs.org/" class="card-link">Officiel Site</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="col-md-6 col-lg-3">
+                        <div className="card dashboard-card">
+                            <img src={redux} className="card-img" alt="Create React App" />
+                            <div className="card-block">
+                                <h3 className="card-title">Redux</h3>
+                                <p class="card-text">A Predictable state container for JavaScript apps</p>
+                                <a href="https://github.com/reduxjs/redux" class="card-link">Github</a>
+                                <a href="https://redux.js.org/" class="card-link">Officiel Site</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-3">
+                        <div className="card dashboard-card">
+                            <img src={reactrouter4} className="card-img" alt="React Router 4" />
+                            <div className="card-block">
+                                <h3 className="card-title">React Router</h3>
+                                <p class="card-text">A collection of navigational components that compose declaratively with a reactJS application</p>
+                                <a href="https://github.com/ReactTraining/react-router" class="card-link">Github</a>
+                                <a href="https://reacttraining.com/react-router/" class="card-link">Officiel Site</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-3">
+                        <div className="card dashboard-card">
+                            <img src={bootstrap4} className="card-img" alt="Bootstrap 4" />
+                            <div className="card-block">
+                                <h3 className="card-title">Bootstrap</h3>
+                                <p class="card-text">An open source toolkit for developing responsive, mobile-first projects on the web with HTML, CSS, and JS</p>
+                                <a href="https://github.com/twbs/bootstrap" class="card-link">Github</a>
+                                <a href="https://getbootstrap.com/" class="card-link">Officiel Site</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
